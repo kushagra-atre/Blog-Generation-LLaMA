bde0db44f430e31ec0fafe6cbee533ceca80c8de
kushagraatre <124891182+kushagra-atre@users.noreply.github.com>
2024-02-02 14:49:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
-## Function To get response from LLAma 2 model
+# Function To get response from LLAma 2 model
 
 def getLLamaresponse(input_text,no_words,blog_style):
 
-    ### LLama2 model
     llm=CTransformers(model='models/llama-2-7b-chat.Q4_k_M.gguf',
                       model_type='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
     
-    ## Prompt Template
+
 
     template="""
         Write a blog for {blog_style} job profile for a topic {input_text}
@@ -22,7 +21,7 @@ def getLLamaresponse(input_text,no_words,blog_style):
     prompt=PromptTemplate(input_variables=["blog_style","input_text",'no_words'],
                           template=template)
     
-    ## Generate the ressponse from the LLama 2 model
+
     response=llm(prompt.format(blog_style=blog_style,input_text=input_text,no_words=no_words))
     print(response)
     return response
@@ -30,7 +29,7 @@ def getLLamaresponse(input_text,no_words,blog_style):
 
 
 
-
+#Streamlit UI
 
 st.set_page_config(page_title="Generate Blogs",
                     page_icon='🤖',
@@ -41,7 +40,6 @@ st.header("Generate Blogs 🤖")
 
 input_text=st.text_input("Enter the Blog Topic")
 
-## creating to more columns for additonal 2 fields
 
 col1,col2=st.columns([5,5])
 
@@ -53,6 +51,5 @@ with col2:
     
 submit=st.button("Generate")
 
-## Final response
 if submit:
     st.write(getLLamaresponse(input_text,no_words,blog_style))
